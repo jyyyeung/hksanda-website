91ee6262354e9f20c1a98816def33456263587d2
YYYeung <sheepyy039@gmail.com>
2022-01-21 19:22:53 +0800

    removed prerender because setup comlicated on heroku

--- vue.config.cjs
+++ vue.config.cjs
@@ -1,24 +1,24 @@
 module.exports = {
   transpileDependencies: ["vue-meta"],
   pluginOptions: {
-    // prerenderSpa: {
-    //   registry: undefined,
-    //   renderRoutes: [
-    //     "/",
-    //     "/about",
-    //     "/contact",
-    //     "/gallery/training",
-    //     "/gallery/videos",
-    //     "/course/content",
-    //     "/course/session",
-    //     "/course/class",
-    //     "/about/our-team",
-    //     "/about/media-interviews",
-    //   ],
-    //   useRenderEvent: false,
-    //   headless: true,
-    //   onlyProduction: true,
-    // },
+    prerenderSpa: {
+      //   registry: undefined,
+      //   renderRoutes: [
+      //     "/",
+      //     "/about",
+      //     "/contact",
+      //     "/gallery/training",
+      //     "/gallery/videos",
+      //     "/course/content",
+      //     "/course/session",
+      //     "/course/class",
+      //     "/about/our-team",
+      //     "/about/media-interviews",
+      //   ],
+      //   useRenderEvent: false,
+      //   headless: true,
+      //   onlyProduction: true,
+    },
     apollo: {
       lintGQL: true,
     },
