a598c1d9d73f515f5c541cc423430fe328072276
YYYeung <sheepyy039@gmail.com>
2023-06-04 14:20:56 +0800

    Remove cache to respect gitignore

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/yyymx/Dev/Projects/Current/hksanda-website/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
--- node_modules/nitropack/dist/runtime/routes/openapi.d.ts
+++
@@ -1,3 +0,0 @@
-import type { OpenAPI3 } from "openapi-typescript";
-declare const _default: import("h3").EventHandler<OpenAPI3>;
-export default _default;
