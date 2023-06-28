7058f9080d4586121bfdc5e894d6b571ebddd19d
YYYeung <sheepyy039@gmail.com>
2023-06-28 14:19:14 +0800

    Contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -40,6 +40,76 @@
         </ClientOnly>
       </div>
     </div>
+    <div class="container mx-auto max-w-2xl">
+      <h2>立即查詢</h2>
+      <!-- component -->
+      <form class="w-full">
+        <div class="-mx-3 mb-6 flex flex-wrap">
+          <div class="mb-6 w-full px-3 md:mb-0 md:w-1/2">
+            <label
+              class="mb-2 block font-bold uppercase tracking-wide"
+              for="grid-first-name"
+            >
+              稱呼
+            </label>
+            <input
+              class="mb-3 block w-full appearance-none rounded px-4 py-3 leading-tight focus:border-primary focus:ring-primary"
+              id="grid-first-name"
+              type="text"
+              placeholder=""
+              v-model="name"
+            />
+          </div>
+          <div class="mb-6 w-full px-3 md:mb-0 md:w-1/2">
+            <label for="course" class="mb-2 block font-medium">
+              查詢課程
+            </label>
+            <select
+              id="course"
+              class="block w-full rounded-lg p-2.5 focus:border-primary focus:ring-primary"
+              v-model="course"
+            >
+              <option>散打自衛術課程</option>
+              <option>恆常團體班</option>
+              <option v-for="course in courses">{{ course?.name }}</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="-mx-3 mb-6 flex flex-wrap">
+          <div class="w-full px-3">
+            <label
+              class="mb-2 block font-bold uppercase tracking-wide"
+              for="grid-password"
+            >
+              查詢內容
+            </label>
+            <textarea
+              class="no-resize mb-3 block h-48 w-full resize-none appearance-none rounded px-4 py-3 leading-tight focus:border-primary focus:ring-primary"
+              id="message"
+              v-model="message"
+            ></textarea>
+          </div>
+        </div>
+
+        <div class="md:flex md:items-center">
+          <div class="md:w-1/3">
+            <button :class="`btn-primary `" type="button">
+              <a
+                v-bind:href="`https://wa.me/85297322082?text=${encodeURI(
+                  `稱呼: ${name} \n查詢課程: ${course} \n查詢內容: ${message}`
+                )}`"
+                target="_blank"
+                class="no-highlight"
+              >
+                <Icon name="fa6-brands:whatsapp" /> 發送
+              </a>
+            </button>
+          </div>
+          <div class="md:w-2/3"></div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -51,4 +121,12 @@ useSeoMeta({
 });
 
 const { data: contacts, pending } = useSanityQuery(GET_CONTACTS);
+
+const GET_ALL_COURSES: string = groq`*[_type == "course-content" ]`;
+
+const { data: courses } = useSanityQuery(GET_ALL_COURSES);
+
+const message = ref("");
+const name = ref("");
+const course = ref("");
 </script>
