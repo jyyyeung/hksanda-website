4e47019c6df168593af801086b02c10f41d9bfc6
YYYeung <sheepyy039@gmail.com>
2023-08-28 18:10:17 +0800

    Added Width for image, and added blur

--- plugins/sanity-image-builder.ts
+++ plugins/sanity-image-builder.ts
@@ -1,11 +1,11 @@
-import imageUrlBuilder from '@sanity/image-url'
+import imageUrlBuilder from "@sanity/image-url";
 
 export default defineNuxtPlugin(() => {
-  const builder = imageUrlBuilder(useSanity().config)
+  const builder = imageUrlBuilder(useSanity().config);
   function urlFor(source) {
-    return builder.image(source).auto('format')
+    return builder.image(source).auto("format");
   }
   return {
     provide: { urlFor },
-  }
-})
+  };
+});
